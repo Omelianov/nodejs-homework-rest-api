642deea422ad04cc1c4a1e4aa32dd670e1dcea25
Omelianov Nicolai <omelianov.n@gmail.com>
2023-08-15 18:33:22 +0300

    Update getCurrent.js

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,24 +1,23 @@
-// const getCurrent = async(req, res) => {
-//     const {name, email} = req.user;
-
-//     res.json({
-//         name,
-//         email,
-//     })
-// }
-
-// module.exports = getCurrent;
-
-
-
-
 const getCurrent = async (req, res) => {
-    const { email, subscription } = req.user;
+    const { name, email } = req.user;
 
     res.json({
+        name,
         email,
-        subscription,
     })
 }
+
+
+
+
+
+// const getCurrent = async (req, res) => {
+//     const { email, subscription } = req.user;
+
+//     res.json({
+//         email,
+//         subscription,
+//     })
+// }
 
 module.exports = getCurrent;
